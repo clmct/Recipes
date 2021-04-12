3a31c48df86a5fc80d36b7f97ab87edc656f4bb2
Almat Kulbaev <almat.kulbaev@yandex.ru>
2021-04-12 21:56:54 +0700

    initial commit

---
+++ Recipes/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Recipes
+//
+//  Created by Almat Kulbaev on 12.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBlue
+  }
+
+}
+
